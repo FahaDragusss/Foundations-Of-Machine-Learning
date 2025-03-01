bb06dbffe37320a99b32e8241e4ad6258dc1fd79
FahaDragusss <fahadjawedmolai03@gmail.com>
2025-03-01 21:29:03 +0100

    Gradient descent

--- Linear Regression from Scratch/Gradient descent.py
+++ Linear Regression from Scratch/Gradient descent.py
@@ -5,4 +5,6 @@ def gradient_descent(theta,X,y_pred,target,lr):
         sum = 0
         for i in range(len(X)):
             sum += (target[i] - y_pred[i])*X[i][j]
+        theta[j] = theta[j] - (lr*(sum))/len(target)
+    return theta
         
