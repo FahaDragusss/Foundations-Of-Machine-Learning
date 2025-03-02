f3b5a4f5ddf98cdc96ea7a1f25d238b595ac064c
FahaDragusss <fahadjawedmolai03@gmail.com>
2025-03-03 00:16:27 +0100

    House prediction model Massively improved minor adjustments still pending

--- Linear Regression from Scratch/Gradient descent.py
+++ Linear Regression from Scratch/Gradient descent.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-def batch_gradient_descent(theta , X_train , y_pred , y_train , lr):
+def batch_gradient_descent(theta , X_train , y_train , lr):
     y_train = y_train.to_numpy()
     y_pred = np.dot(X_train , theta)  
 
@@ -11,7 +11,7 @@ def batch_gradient_descent(theta , X_train , y_pred , y_train , lr):
         for i in range(len(X_train)):
             sum += (y_train[i] - y_pred[i])*X_train[i, j]
             
-        theta[j] = theta[j] - (lr*(sum))/len(y_test)
+        theta[j] = theta[j] - (lr*(sum))/len(y_train)
 
     return theta
         
